6200301b93543fb890c8caea79ba2e7702554c3d
Maksim Krasutski <krasutskimaksim@gmail.com>
2025-05-27 10:23:28 +0200

    Add ScrollZoom.cs

---
+++ Assets/ScrollZoom.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using Unity.Cinemachine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CinemachineCamera))]
+public class ScrollZoom : MonoBehaviour
+{
+    [Header("Zoom Settings")]
+    [SerializeField] private InputActionReference _scroll;
+    [SerializeField] private float _zoomSpeed = 10f;
+    [SerializeField] private float _minZoom = 2f;
+    [SerializeField] private float _maxZoom = 20f;
+
+    private CinemachineFollow _cinemachineFollow;
+
+    private void Awake()
+    {
+        _cinemachineFollow = GetComponent<CinemachineFollow>();
+    }
+
+    private void OnEnable()
+    {
+        _scroll.action.performed += Zoom;
+    }
+
+    private void OnDisable()
+    {
+        _scroll.action.performed -= Zoom;
+    }
+    
+    private void Zoom(InputAction.CallbackContext ctx)
+    {
+        float scrollInput = ctx.ReadValue<Vector2>().y;
+
+        Vector3 offset = _cinemachineFollow.FollowOffset;
+        offset.y += scrollInput * -_zoomSpeed;
+        offset.y = Mathf.Clamp(offset.y, _minZoom, _maxZoom);
+        _cinemachineFollow.FollowOffset = offset;
+    }
+}
